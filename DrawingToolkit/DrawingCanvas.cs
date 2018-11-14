b81364b89d49bfce8521ff61d1c5902368a96004
Unknown <widheramahana@gmail.com>
2018-11-14 12:11:45 +0700

    add group object

--- DrawingToolkit/DrawingCanvas.cs
+++ DrawingToolkit/DrawingCanvas.cs
@@ -4,20 +4,24 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DrawingToolkit
 {
-    public class DrawingCanvas:Panel
+    public class DrawingCanvas : Panel
     {
         private Point start;
         private Point end;
         private int flag = 0;
+        private int buttonFlag = 0;
         private int tool = 0;
+        private bool group = false;
         private Point positionTemp;
         private DrawingObject selectObject;
+        private List<DrawingObject> groupObject = new List<DrawingObject>();
         private List<DrawingObject> allObject = new List<DrawingObject>();
         public DrawingCanvas()
         {
@@ -35,7 +39,7 @@ namespace DrawingToolkit
 
         public void panel1_Paint(object sender, PaintEventArgs e)
         {
-            
+
             if (flag == 1 && tool == 1)
             {
                 Line line = new Line(start, end);
@@ -46,7 +50,7 @@ namespace DrawingToolkit
             {
                 Square square = new Square(start, end);
                 square.Draw(e);
-                
+
             }
 
             else if (flag == 1 && tool == 3)
@@ -81,52 +85,115 @@ namespace DrawingToolkit
                 Circle circle = new Circle(start, end);
                 allObject.Add(circle);
             }
-            if (tool == 4)
-            {
-                if(selectObject!=null)
-                {
-                    Pen pen = new Pen(Color.Black);
-                    selectObject.ChangeColor(pen);
-                    selectObject = null;
-                    this.Refresh();
-                }
-                
-            }
 
         }
 
         public void panel1_MouseDown(object sender, MouseEventArgs e)
         {
-            if(tool!=4)
+            Console.WriteLine(e.Button);
+            if (tool != 4)
             {
+                foreach (var obj in allObject)
+                {
+                    if (obj == selectObject)
+                    {
+                        Pen pen = new Pen(Color.Black);
+                        obj.ChangeColor(pen);
+                        selectObject = null;
+                        break;
+                    }
+                }
                 flag = 1;
                 start = e.Location;
             }
             else if (tool == 4)
             {
-                foreach(var obj in allObject)
+                if (!group)
                 {
-                    if (obj.Intersect(e))
+                    foreach (var obj in allObject)
                     {
-                        var test = e.Location;
-                        positionTemp = e.Location;
-                        //Console.WriteLine("msuk");
-                        selectObject = obj;
-                        this.Refresh();
-                    }
-                    else
-                    {
-                        Pen pen = new Pen(Color.Black);
-                        obj.ChangeColor(pen);
-                        //selectObject = null;
-                        this.Refresh();
-                        //Console.WriteLine("enggak msuk");
+                        if (obj == selectObject)
+                        {
+                            Pen pen = new Pen(Color.Black);
+                            obj.ChangeColor(pen);
+                            selectObject = null;
+                            break;
+                        }
                     }
                 }
-            }
-            
-        }
+                if (e.Button == MouseButtons.Left)
+                {
+                    buttonFlag = 0;
 
+
+                    foreach (var obj in allObject)
+                    {
+                        if (obj.Intersect(e) && !group)
+                        {
+                            var test = e.Location;
+                            positionTemp = e.Location;
+                            selectObject = obj;
+                            //this.Refresh();
+                            break;
+                        }
+                        else if (obj.Intersect(e) && group)
+                        {
+                            var alreadySelected = 0;
+                            foreach (var objGrp in groupObject)
+                            {
+                                if (obj == objGrp)
+                                {
+                                    alreadySelected = 1;
+                                }
+                            }
+                            if (alreadySelected == 1)
+                                break;
+                            var test = e.Location;
+                            positionTemp = e.Location;
+                            //selectObject = obj;
+                            groupObject.Add(obj);
+                            //this.Refresh();
+                            break;
+                        }
+                        else if (!obj.Intersect(e) && !group)
+                        {
+                            Pen pen = new Pen(Color.Black);
+                            obj.ChangeColor(pen);
+                            selectObject = null;
+                            //this.Refresh();
+                            Console.WriteLine("enggak msuk");
+                        }
+                    }
+                    this.Refresh();
+                }
+                else if (e.Button == MouseButtons.Right)
+                {
+                    if (group)
+                    {
+                        group = false;
+                        foreach (var obj in allObject)
+                        {
+                            foreach (var objGrp in groupObject)
+                            {
+                                if (obj == objGrp)
+                                {
+                                    Pen pen = new Pen(Color.Black);
+                                    obj.ChangeColor(pen);
+                                    selectObject = null;
+                                }
+                            }
+                        }
+                        groupObject.Clear();
+                        this.Refresh();
+                    }
+
+                    else
+                        group = true;
+
+                }
+
+            }
+        }
         public void panel1_MouseMove(object sender, MouseEventArgs e)
         {
             if (flag == 1)
@@ -134,19 +201,38 @@ namespace DrawingToolkit
                 end = e.Location;
                 this.Refresh();
             }
-            if(selectObject!=null)
+            if (selectObject != null && !group)
             {
                 int xAmount = e.X - positionTemp.X;
                 int yAmount = e.Y - positionTemp.Y;
+                Console.WriteLine(xAmount + " " + yAmount);
                 positionTemp = e.Location;
-                selectObject.MoveShape(e, xAmount, yAmount);
-                this.Refresh(); 
+                if (e.Button == MouseButtons.Left)
+                {
+                    selectObject.MoveShape(e, xAmount, yAmount);
+                }
+               
+                //selectObject.MoveShape(e, xAmount, yAmount);
+                this.Refresh();
+            }
+            else if (selectObject == null && group)
+            {
+                int xAmount = e.X - positionTemp.X;
+                int yAmount = e.Y - positionTemp.Y;
+                Console.WriteLine("haii" + groupObject.Count);
+                positionTemp = e.Location;
+                if (e.Button == MouseButtons.Left)
+                {
+                    foreach (var obj in groupObject)
+                        obj.MoveShape(e, xAmount, yAmount);
+                }
+                this.Refresh();
             }
         }
         public void setToolValue(int value)
         {
             tool = value;
         }
-        
+
     }
 }
